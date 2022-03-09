1ba4bb7115a1ca2788f3c2a8e75dd5a126d8d3fe
carganspearing123 <99367024+carganspearing123@users.noreply.github.com>
2022-03-10 10:56:48 +1300

    Add files via upload

--- 00_LU_Base_v_01.py
+++ 00_LU_Base_v_01.py
@@ -1,4 +1,4 @@
-
+import random
 
 # Functions go here ...
 def yes_no(question):
@@ -56,6 +56,52 @@ print()
 # ask the user how much they want to play with
 
 how_much = num_check("How much would you like to play with? ", 0, 10)
+
+balance = how_much
+
+rounds_played = 0
+
+play_again = input("Press <Enter> to play...").lower()
+while play_again == "":
+    
+    #increase # of rounds played
+    rounds_played += 1
+
+    # print round number
+    print()
+    print("*** Rounds #{} ***".format(rounds_played))
+  
+    chosen_num = random.randint(1, 100)
+   
+    #adjust balance
+    if 1 <= chosen_num <= 5:
+        chosen == "unicorn"
+        balance += 4    
+
+        
+    elif 6 <= chosen_num <= 36: 
+        chosen == "donkey"
+        balance -= 1
+    else:
+        if chosen_num % 2 == 0:
+         chosen = "horse"
+        else:
+            chosen = "zebra"
+        balance -= 0.5
+
+    print("You got a {}. Your balance is"
+          "${:.2f} ".format(chosen, balance))
+
+
+    if balance < 1:
+        play_again = "xxx"
+        print("Sorry you have run out of money")
+    else:
+     play_again = input("Please Enter to play again "
+                        "or 'xxx' to quit")
+print()
+print("Final balance", balance)
+
 
 print()
 
---
+++ 06_Rounds_v01.py
@@ -0,0 +1,19 @@
+balance = 5
+rounds_played = 0
+play_again = input("Press <Enter> to play...")
+while play_again == "":
+    #increase # of rounds played
+    rounds_played += 1
+    # print round number
+    print("*** Rounds #{}".format(rounds_played))
+    balance -= 1
+    print("Balance: ", balance)
+    print()
+    if balance < 1:
+        play_again = "xxx"
+        print("Sorry you have run out of money")
+    else:
+     play_again = input("Please Enter to play again "
+                        "or 'xxx' to quit")
+print()
+print("Final balance", balance)
